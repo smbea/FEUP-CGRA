51ce30ef098acc635adabfc40c80410ce71de774
Beatriz <bea.smendes98@gmail.com>
2018-02-20 22:22:15 +0000

    Up until exercise 3.5.

--- tp1/TPscene.js
+++ tp1/TPscene.js
@@ -6,7 +6,7 @@ class TPscene extends CGFscene
     }
 
 
-    init(application) 
+    init(application)
     {
         super.init(application);
 
@@ -28,7 +28,7 @@ class TPscene extends CGFscene
         // NOTE: OpenGL transformation matrices are transposed
 
         // Translate (5, 0, 2)
-        
+
         this.tra = [  1.0, 0.0, 0.0, 0.0,
                     0.0, 1.0, 0.0, 0.0,
                     0.0, 0.0, 1.0, 0.0,
@@ -37,7 +37,7 @@ class TPscene extends CGFscene
         // Rotate 30 degrees around Y
         // These constants would normally be pre-computed at initialization time
         // they are placed here just to simplify the example
-        
+
         this.deg2rad=Math.PI/180.0;
         var a_rad = 30.0 * this.deg2rad;
         var cos_a = Math.cos(a_rad);
@@ -57,33 +57,33 @@ class TPscene extends CGFscene
 
     };
 
-    initLights() 
+    initLights()
     {
 
         this.lights[0].setPosition(15, 2, 5, 1);
         this.lights[0].setDiffuse(1.0,1.0,1.0,1.0);
         this.lights[0].enable();
         this.lights[0].update();
-    
+
     };
 
-    initCameras() 
+    initCameras()
     {
         this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(15, 15, 15), vec3.fromValues(0, 0, 0));
     };
 
-    setDefaultAppearance() 
+    setDefaultAppearance()
     {
         this.setAmbient(0.2, 0.4, 0.8, 1.0);
         this.setDiffuse(0.2, 0.4, 0.8, 1.0);
         this.setSpecular(0.2, 0.4, 0.8, 1.0);
-        this.setShininess(10.0);	
+        this.setShininess(10.0);
     };
 
-    display() 
+    display()
     {
         // ---- BEGIN Background, camera and axis setup
-        
+
         // Clear image and depth buffer everytime we update the scene
         this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
         this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
@@ -99,24 +99,33 @@ class TPscene extends CGFscene
         this.axis.display();
 
         this.setDefaultAppearance();
-        
+
         // ---- END Background, camera and axis setup
 
-        
+
         // ---- BEGIN Geometric transformation section
 
-        // Multiplication of the previous transformations
+        this.pushMatrix();
+
+        this.translate(0,5,0);
+        this.obj.display();
+
+        //Multiplication of the previous transformations
         //this.multMatrix(this.tra);     // GT = GT * tra
         //this.multMatrix(this.rot);     // GT = GT * rot
         //this.multMatrix(this.sca);     // GT = GT * sca
 
+        this.popMatrix();
+        this.scale(5,2,1);
+        this.translate(5,0,2);
+
         // ---- END Geometric transformation section
-        
+
 
         // ---- BEGIN Primitive drawing section
 
         this.obj.display();
-        
+
         // ---- END Primitive drawing section
 
     };
