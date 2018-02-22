6ffcd7084c3e691dfd6ce54b8bf07f5b1681f4a5
António Cruz <acruz9823@gmail.com>
2018-02-22 12:41:48 +0000

    Update MyUnitCube.js

--- tp1/MyUnitCube.js
+++ tp1/MyUnitCube.js
@@ -1,53 +1,53 @@
-/**
+//**
  * MyUnitCube
  * @param gl {WebGLRenderingContext}
  * @constructor
  */
 
+ class MyUnitCube extends CGFobject
+ {
+ 	constructor(scene)
+ 	{
+ 		super(scene);
+ 		this.initBuffers();
+ 	};
 
-class MyUnitCube extends CGFobject
-{
-	constructor(scene)
-	{
-		super(scene);
-		this.initBuffers();
-	};
-
-	initBuffers()
-	{
-		this.vertices = [
-				-0.5, -0.5, -0.5,
-				-0.5, 0.5, -0.5,
-				0.5, -0.5, -0.5,
-				0.5, 0.5, -0.5,
+ 	initBuffers()
+ 	{
+ 		this.vertices = [
+ 				-0.5, -0.5, -0.5,
+ 				0.5, -0.5, -0.5,
+ 				0.5, 0.5, -0.5,
+ 				-0.5, 0.5, -0.5,
         -0.5, -0.5, 0.5,
-				-0.5, 0.5, 0.5,
-				0.5, -0.5, 0.5,
-				0.5, 0.5, 0.5,
-				];
+ 				0.5, -0.5, 0.5,
+ 				0.5, 0.5, 0.5,
+ 				-0.5, 0.5, 0.5,
+ 				];
 
-		this.indices = [
-				1,2,0,
-        1,3,2,
+ 		this.indices = [
+        //
+ 				2,1,0,
+        0,3,2,
+        //
+        4,5,6,
+        6,7,4,
+        //
+        2,3,7,
+        6,2,7,
+        //
+        4,3,0,
+        7,3,4,
+        //
+        1,2,6,
+        5,1,6,
+        //
+        5,0,1,
+        5,4,0,
 
-        3,7,2,
-        2,7,6,
+ 			];
 
-        1,7,3,
-        1,5,7,
-
-        7,4,6,
-        5,4,7,
-
-        2,6,0,
-        0,6,4,
-
-        4,5,1,
-        1,0,4
-
-			];
-
-		this.primitiveType=this.scene.gl.TRIANGLES;
-		this.initGLBuffers();
-	};
-};
+ 		this.primitiveType=this.scene.gl.TRIANGLES;
+ 		this.initGLBuffers();
+ 	};
+ };
