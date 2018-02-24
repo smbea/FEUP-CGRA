a6f0bc0f501c66f57beafa8aad3a553299d01f72
Beatriz <bea.smendes98@gmail.com>
2018-02-24 23:58:32 +0000

    Fixed myTable

--- tp1/3.5/MyQuad.js
+++ tp1/3.5/MyQuad.js
@@ -4,7 +4,7 @@
  * @constructor
  */
 
-class MyObject extends CGFobject
+class MyQuad extends CGFobject
 {
 	constructor(scene)
 	{
@@ -19,15 +19,11 @@ class MyObject extends CGFobject
 				0.5, -0.5, 0,
 				-0.5, 0.5, 0,
 				0.5, 0.5, 0,
-				-1, 0.5, 0,
-				1, 0.5, 0,
-				0,1.5,0
 				];
 
 		this.indices = [
 				0, 1, 2,
 				3, 2, 1,
-				4,5,6
 			];
 
 		this.primitiveType=this.scene.gl.TRIANGLES;
