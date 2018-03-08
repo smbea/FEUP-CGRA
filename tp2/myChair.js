bda7310b511a1cfd0b1a90be3df6c77c60fe485b
Antonio <acruz9823@gmail.com>
2018-03-08 22:52:58 +0000

    Finishe Extra and TP02 feito tudo sem ajuda da Cosima :)

--- tp2/myChair.js
+++ tp2/myChair.js
@@ -1,17 +1,16 @@
 /**
- * myTable
+ * myLeg
  * @param gl {WebGLRenderingContext}
  * @constructor
  */
 
- class MyTable extends CGFobject
+ class myChair extends CGFobject
  {
  	constructor(scene)
  	{
  		super(scene);
-    this.cube=new MyUnitCubeQuad(this.scene);
+    this.cube = new MyUnitCubeQuad(this.scene);
     this.leg = new myLeg(this.scene);
-
     this.scene.materialWood = new CGFappearance(this.scene);
     this.scene.materialWood.setAmbient(0.3,0.3,0.3,1);
     this.scene.materialWood.setDiffuse(0.6,0.3,0,1);
@@ -20,32 +19,49 @@
  	};
   display()
   {
+    this.deg2rad=Math.PI/180.0;
 
     this.scene.pushMatrix();
-    this.scene.translate(2.5-0.15,0,-1.5+0.15);
+    this.scene.translate(-1,0,1);
+    this.scene.scale(1,1.3,1);
     this.leg.display();
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
-    this.scene.translate(2.5-0.15,0,1.5-0.15);
+    this.scene.translate(1,0,1);
+    this.scene.scale(1,1.3,1);
     this.leg.display();
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
-    this.scene.translate(-2.5+0.15,0,1.5-0.15);
+    this.scene.translate(-1,0,-1);
+    this.scene.scale(1,0.7,1);
     this.leg.display();
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
-    this.scene.translate(-2.5+0.15,0,-1.5+0.15);
+    this.scene.translate(1,0,-1);
+    this.scene.scale(1,0.7,1);
+    this.leg.display();
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+    this.scene.translate(1,0,-1);
+    this.scene.scale(1,0.7,1);
+    this.leg.display();
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+    this.scene.rotate(90 * this.deg2rad, 0,0,1);
+    this.scene.scale(0.5,0.658,1)
+    this.scene.translate(9,-1.75,1);5
     this.leg.display();
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
     this.scene.materialWood.apply();
-    this.scene.translate(0,3.5,0);
-    this.scene.scale(5,0.3,3);
-    this.scene.translate(0,0.5,0);
+    this.scene.translate(0,2.5,0);
+    this.scene.scale(2 + 0.5 * (0.3 * 2),0.5,2 + 0.5 * (0.3 * 2));
     this.cube.display();
     this.scene.popMatrix();
   }
