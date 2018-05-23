57974d45c229c772ab96c880058144d692b0292a
Antonio <acruz9823@gmail.com>
2018-05-23 10:12:00 +0100

    Guindastre sem esparguete

--- proj/src/MyCrane.js
+++ proj/src/MyCrane.js
@@ -9,8 +9,12 @@ class MyCrane extends CGFobject{
 		this.leftAnimation = true;
 		this.rightAnimation = false;
 		this.moving = false;
-		this.firstPillarDegree = Math.PI/10;
+		this.firstPillarDegree = Math.PI/5.2;
+		this.firstPillarSize = 8;
 		this.secondPillarDegree = -Math.PI / 2.5;
+		this.secondPillarSize = 4;
+		this.stringSize = 2;
+		//END this.secondPillarDegree = -Math.PI / 6
 		this.secondPillarHeight = 0;
 		this.hasCar = false;
 	};
@@ -18,98 +22,84 @@ class MyCrane extends CGFobject{
 	display()
 	{
 		this.scene.pushMatrix();
-		this.scene.translate(-6,0,-4);
+		//this.scene.translate(-6,0,-4);
 		//Base of Crane
     this.scene.pushMatrix();
     this.scene.scale(2,1,2);
     this.cylinder.display();
     this.scene.popMatrix();
+		this.scene.translate(0,1,0);
 
 		this.scene.pushMatrix();
 		this.scene.rotate(this.orientation,0,1,0);
 
 		//First Turning Cilinder
 		this.scene.pushMatrix();
-		this.scene.translate(0,1,0);
     this.cylinder.display();
     this.scene.popMatrix();
 
+		this.scene.pushMatrix();
+		this.scene.translate(0,0.5,0);
 		//First Pillar
 		this.scene.pushMatrix();
-		this.scene.translate(0,1.8,0);
 		this.scene.rotate(this.firstPillarDegree,1,0,0);
-		this.scene.scale(0.8,8,0.8);
+		this.scene.scale(0.8,this.firstPillarSize,0.8);
 		this.scene.translate(0,0.5,0);
 		this.body.display();
 		this.scene.popMatrix();
 
 		//Second Turning Cilinder
 		this.scene.pushMatrix();
-		this.scene.translate(0.5,9.5 + (this.secondPillarHeight / 2.5),2.5 - ((this.secondPillarHeight / 1.5) * Math.cos(this.secondPillarDegree)));
+		this.scene.translate(0,this.firstPillarSize * Math.cos(this.firstPillarDegree),this.firstPillarSize * Math.sin(this.firstPillarDegree));
 		this.scene.rotate(Math.PI/2,0,0,1);
 		this.scene.scale(0.8,1,0.8);
+		this.scene.translate(0,-0.5,0);
     this.cylinder.display();
     this.scene.popMatrix();
 
 		//Second Pillar
 		this.scene.pushMatrix();
-		this.scene.translate(0,8.2 + this.secondPillarHeight,6.5);
-		this.scene.rotate(this.secondPillarDegree,1,0,0);
-		this.scene.scale(0.8,4,0.8);
+		//this.scene.translate(0,7,8.4);
+		this.scene.translate(0,this.firstPillarSize * Math.cos(this.firstPillarDegree),this.firstPillarSize * Math.sin(this.firstPillarDegree));
+		this.scene.rotate(this.secondPillarDegree + Math.PI,1,0,0);
+		this.scene.scale(0.8,this.secondPillarSize,0.8);
 		this.scene.translate(0,0.5,0);
 		this.body.display();
 		this.scene.popMatrix();
 
 		//String attached to the magnet
 		this.scene.pushMatrix();
-		this.scene.translate(0,5.2 + this.secondPillarHeight,6.5);
-		this.scene.scale(0.05,2,0.05);
-		this.scene.translate(0,0.5,0);
+	  this.scene.translate(0,this.firstPillarSize * Math.cos(this.firstPillarDegree) - this.secondPillarSize * Math.cos(this.secondPillarDegree)  ,this.firstPillarSize * Math.sin(this.firstPillarDegree)- this.secondPillarSize * Math.sin(this.secondPillarDegree) );
+		this.scene.scale(0.05,this.stringSize,0.05);
+		this.scene.translate(0,-1,0);
 		this.cylinder.display();
 		this.scene.popMatrix();
 
 		//Magnet
 		this.scene.pushMatrix();
-		this.scene.translate(0,5.7 + this.secondPillarHeight,6.5);
+		 this.scene.translate(0,this.firstPillarSize * Math.cos(this.firstPillarDegree) - this.secondPillarSize * Math.cos(this.secondPillarDegree) - this.stringSize,this.firstPillarSize * Math.sin(this.firstPillarDegree)- this.secondPillarSize * Math.sin(this.secondPillarDegree) );
 		this.scene.scale(1.4,0.5,1.4);
-		this.scene.translate(0,0.5,0);
+		this.scene.translate(0,-0.5,0);
 		this.cylinder.display();
 		this.scene.popMatrix();
 
 		this.scene.popMatrix();
 		this.scene.popMatrix();
+		this.scene.popMatrix();
 
 	};
 	update(){
-		if(this.moving && this.leftAnimation){
-			this.firstPillarDegree += Math.PI/130;
-			this.secondPillarDegree += Math.PI/50;
-			this.secondPillarHeight -= 0.3;
-			if(this.firstPillarDegree >= Math.PI / 5.2){
-				this.firstPillarDegree = Math.PI / 5.2;
+		if(this.moving){
+			this.orientation+= Math.PI/50;
+			if(this.orientation > Math.PI){
+				this.orientation = Math.PI;
+				this.secondPillarDegree += Math.PI/50;
+				if(this.secondPillarDegree >= -Math.PI / 6){
+					this.secondPillarDegree = -Math.PI / 6;
+				}
+
 			}
-			if(this.secondPillarDegree >= -Math.PI / 6){
-				this.secondPillarDegree = -Math.PI / 6;
-			}
-			if(this.secondPillarHeight <= -3.5){
-				this.secondPillarHeight = -3.5
-			}
-			if(this.hasCar){
-				this.orientation += Math.PI/50;
-			}
-		}
-		if(this.moving && this.rightAnimation){
-			this.orientation -= Math.PI/50;
-		}
-		if(this.orientation >= Math.PI){
-			this.moving = false;
-			this.leftAnimation = false;
-			this.rightAnimation = true;
-		}
-		if(this.orientation < 0){
-			this.moving = false;
-			this.leftAnimation = true;
-			this.rightAnimation = false;
+
 		}
 	}
 
